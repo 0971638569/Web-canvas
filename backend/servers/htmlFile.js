2f8e6ea57c5baa21b1d37a52a3ea8e90f3bf4618
PHUC <phuc270994@gmail.com>
2023-03-10 16:48:49 +0700

    Use html file

--- backend/servers/htmlFile.js
+++ backend/servers/htmlFile.js
@@ -4,21 +4,25 @@ const fs = require('fs').promises;
 const host = "localhost";
 const port = 8000;
 
+let indexFile;
+
 const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
-    .then(content => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(content);
-    })
-    .catch(err => {
-        res.writeHead(500);
-        res.end(err);
-        return;
-    });
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+fs.readFile(__dirname + "/modules/index.html")
+    .then(content => {
+        indexFile = content;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`)
+        res.writeHead(500);
+        res.end(err);
+        process.exit(1);
+    });
--- start.bat
+++ start.bat
@@ -2,5 +2,6 @@
 @REM call node backend/servers/json.js
 @REM call node backend/servers/csv.js
 @REM call node backend/servers/html.js
-start cmd.exe /C "copy Projects\mjo_20\WebApp\wwwroot\css content-server\public\mjo\assets\css"
-call node backend/servers/htmlFile.js
+start cmd.exe /C "xcopy frontend\ backend\servers\modules /E /y"
+@REM call node backend/servers/htmlFile.js
+call node backend/servers/routes.js
